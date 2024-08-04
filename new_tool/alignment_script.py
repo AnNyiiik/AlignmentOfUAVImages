01f8a6ec7c5522e9a152023837dbe56333cbb12f
AnNyiiik <anna.nikolaevakons@gmail.com>
2024-08-04 13:46:01 +0200

    added CNN based class

--- new_tool/alignment_script.py
+++ new_tool/alignment_script.py
@@ -1,5 +1,5 @@
 import homography_finder_feature_based
+import homography_finder_CNN_based
 
-homography_finder = homography_finder_feature_based.homography_finder_feature_based()
-H = homography_finder.align("/Users/annnikolaeff/Desktop/uav_after_resize.png", "/Users/annnikolaeff/Desktop/satellite_after_resize.png")
-print(H[0])
+homography_finder = homography_finder_CNN_based.homography_finder_CNN_based()
+homography_finder.align("/Users/annnikolaeff/Desktop/uav_after_resize.png", "/Users/annnikolaeff/Desktop/satellite_after_resize.png")
--- new_tool/homography_finder_CNN_based.py
+++ new_tool/homography_finder_CNN_based.py
@@ -2,6 +2,7 @@ import cv2 as cv
 import numpy as np
 import os
 
+from PIL import Image
 from pathlib import Path
 
 import util
@@ -12,4 +13,24 @@ class homography_finder_CNN_based(homography_finder.homography_finder):
     def align(self, img_query_path, img_reference_path):
         img_query_cropped = util.clip_image(img_query_path)
         img_reference_cropped = util.clip_image(img_reference_path)
-        os.system('docker start image_name')
+
+        absolute_path = Path('~/AlignmentOfUAVImages').expanduser()
+        if not absolute_path.joinpath("uav").exists():
+            os.mkdir(absolute_path.joinpath("uav"))
+        query_saved_path = absolute_path.joinpath("uav/query.jpg")
+        img_query_cropped.save(query_saved_path)
+
+        if not absolute_path.joinpath("sat").exists():
+            os.mkdir(absolute_path.joinpath("sat"))
+        reference_saved_path = absolute_path.joinpath("sat/reference.jpg")
+        img_reference_cropped.save(reference_saved_path)
+
+        os.system('docker start 39be30bf5915')
+
+        os.system(f'docker cp {absolute_path.joinpath("uav/query.jpg")} 39be30bf5915:/workspace/unsupervisedDeepHomographyRAL2018/data/query.png')
+        os.system(f'docker cp {absolute_path.joinpath("sat/reference.jpg")} 39be30bf5915:/workspace/unsupervisedDeepHomographyRAL2018/data/reference.png')
+
+        os.system('docker exec -it 39be30bf5915 /bin/bash')
+
+
+
