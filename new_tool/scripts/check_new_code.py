ca0dfed866c6392843124cc029208b05b01c4a1f
AnNyiiik <anna.nikolaevakons@gmail.com>
2024-09-11 22:55:55 +0300

    deleted odd file

--- new_tool/scripts/check_new_code.py
+++
@@ -1,4 +0,0 @@
-import homography_finder_CNN_based
-
-homography_finder_CNN = homography_finder_CNN_based.homography_finder_CNN_based()
-homography_finder_CNN.align("/Users/annnikolaeff/Desktop/gt_google/aer_1.png", "/Users/annnikolaeff/Desktop/gt_google/sat_1.png")
