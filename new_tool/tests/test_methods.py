6be95d7f8e20d886fe4135b0e702aebddab95c91
AnNyiiik <anna.nikolaevakons@gmail.com>
2024-09-12 19:55:21 +0300

    changed tests

--- new_tool/tests/test_methods.py
+++ new_tool/tests/test_methods.py
@@ -1,67 +1,63 @@
-import unittest
-from unittest import TestCase
+import pytest
 import numpy as np
 import os
-
-from parameterized import parameterized, parameterized_class
 
 from scripts import homography_finder_feature_based
 from scripts import homography_finder_CNN_based
 
 
-class TestClass(TestCase):
-    homography_finder_feature = (
-        homography_finder_feature_based.homography_finder_feature_based()
-    )
-    homography_finder_CNN = homography_finder_CNN_based.homography_finder_CNN_based()
+homography_finder_feature = (
+    homography_finder_feature_based.homography_finder_feature_based()
+)
+homography_finder_CNN = homography_finder_CNN_based.homography_finder_CNN_based()
 
-    @parameterized.expand(
-        [
-            (
-                homography_finder_feature,
-                os.path.abspath("exp_data/aer_1.png"),
-                os.path.abspath("exp_data/sat_1.png"),
-            ),
-            (
-                homography_finder_CNN,
-                os.path.abspath("exp_data/aer_1.png"),
-                os.path.abspath("exp_data/sat_1.png"),
-            ),
-        ]
-    )
-    def test_check_homography_search(self, method, query_path, reference_path):
-        H = method.align(query_path, reference_path)
-        assert type(H) == np.ndarray
-        assert H.shape == (3, 3)
+testdata_homography_search = (
+    [
+        (
+            homography_finder_feature,
+            os.path.abspath("exp_data/aer_1.png"),
+            os.path.abspath("exp_data/sat_1.png"),
+        ),
+        (
+            homography_finder_CNN,
+            os.path.abspath("exp_data/aer_1.png"),
+            os.path.abspath("exp_data/sat_1.png"),
+        ),
+    ]
+)
 
-    @parameterized.expand(
-        [
-            (
-                homography_finder_feature,
-                os.path.abspath("exp_data/aer_2.png"),
-                os.path.abspath("exp_data/sat_3.png"),
-            ),
-            (
-                homography_finder_CNN,
-                os.path.abspath("exp_data/aer_2.png"),
-                os.path.abspath("exp_data/sat_3.png"),
-            ),
-            (
-                homography_finder_feature,
-                os.path.abspath("exp_data/aer_3.png"),
-                os.path.abspath("exp_data/sat_2.png"),
-            ),
-            (
-                homography_finder_CNN,
-                os.path.abspath("exp_data/aer_3.png"),
-                os.path.abspath("exp_data/sat_2.png"),
-            ),
-        ]
-    )
-    def test_check_bad_user_input(self, method, query_path, reference_path):
-        with self.assertRaises(FileNotFoundError):
-            method.align(query_path, reference_path)
+@pytest.mark.parametrize("method,query_path,reference_path", testdata_homography_search)
+def test_check_homography_search(method, query_path, reference_path):
+    H = method.align(query_path, reference_path)
+    assert type(H) == np.ndarray
+    assert H.shape == (3, 3)
 
+testdata_bad_input = (
+    [
+        (
+            homography_finder_feature,
+            os.path.abspath("exp_data/aer_2.png"),
+            os.path.abspath("exp_data/sat_3.png"),
+        ),
+        (
+            homography_finder_CNN,
+            os.path.abspath("exp_data/aer_2.png"),
+            os.path.abspath("exp_data/sat_3.png"),
+        ),
+        (
+            homography_finder_feature,
+            os.path.abspath("exp_data/aer_3.png"),
+            os.path.abspath("exp_data/sat_2.png"),
+        ),
+        (
+            homography_finder_CNN,
+            os.path.abspath("exp_data/aer_3.png"),
+            os.path.abspath("exp_data/sat_2.png"),
+        ),
+    ]
+)
 
-if __name__ == "__main__":
-    unittest.main()
+@pytest.mark.parametrize("method,query_path,reference_path", testdata_bad_input)
+def test_check_bad_user_input(method, query_path, reference_path):
+    with pytest.raises(FileNotFoundError):
+        method.align(query_path, reference_path)
