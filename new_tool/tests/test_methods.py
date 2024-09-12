567a8d4324e75eb59629a44b051421f1b623b248
AnNyiiik <anna.nikolaevakons@gmail.com>
2024-09-12 19:55:59 +0300

    reformatted

--- new_tool/tests/test_methods.py
+++ new_tool/tests/test_methods.py
@@ -11,20 +11,19 @@ homography_finder_feature = (
 )
 homography_finder_CNN = homography_finder_CNN_based.homography_finder_CNN_based()
 
-testdata_homography_search = (
-    [
-        (
-            homography_finder_feature,
-            os.path.abspath("exp_data/aer_1.png"),
-            os.path.abspath("exp_data/sat_1.png"),
-        ),
-        (
-            homography_finder_CNN,
-            os.path.abspath("exp_data/aer_1.png"),
-            os.path.abspath("exp_data/sat_1.png"),
-        ),
-    ]
-)
+testdata_homography_search = [
+    (
+        homography_finder_feature,
+        os.path.abspath("exp_data/aer_1.png"),
+        os.path.abspath("exp_data/sat_1.png"),
+    ),
+    (
+        homography_finder_CNN,
+        os.path.abspath("exp_data/aer_1.png"),
+        os.path.abspath("exp_data/sat_1.png"),
+    ),
+]
+
 
 @pytest.mark.parametrize("method,query_path,reference_path", testdata_homography_search)
 def test_check_homography_search(method, query_path, reference_path):
@@ -32,30 +31,30 @@ def test_check_homography_search(method, query_path, reference_path):
     assert type(H) == np.ndarray
     assert H.shape == (3, 3)
 
-testdata_bad_input = (
-    [
-        (
-            homography_finder_feature,
-            os.path.abspath("exp_data/aer_2.png"),
-            os.path.abspath("exp_data/sat_3.png"),
-        ),
-        (
-            homography_finder_CNN,
-            os.path.abspath("exp_data/aer_2.png"),
-            os.path.abspath("exp_data/sat_3.png"),
-        ),
-        (
-            homography_finder_feature,
-            os.path.abspath("exp_data/aer_3.png"),
-            os.path.abspath("exp_data/sat_2.png"),
-        ),
-        (
-            homography_finder_CNN,
-            os.path.abspath("exp_data/aer_3.png"),
-            os.path.abspath("exp_data/sat_2.png"),
-        ),
-    ]
-)
+
+testdata_bad_input = [
+    (
+        homography_finder_feature,
+        os.path.abspath("exp_data/aer_2.png"),
+        os.path.abspath("exp_data/sat_3.png"),
+    ),
+    (
+        homography_finder_CNN,
+        os.path.abspath("exp_data/aer_2.png"),
+        os.path.abspath("exp_data/sat_3.png"),
+    ),
+    (
+        homography_finder_feature,
+        os.path.abspath("exp_data/aer_3.png"),
+        os.path.abspath("exp_data/sat_2.png"),
+    ),
+    (
+        homography_finder_CNN,
+        os.path.abspath("exp_data/aer_3.png"),
+        os.path.abspath("exp_data/sat_2.png"),
+    ),
+]
+
 
 @pytest.mark.parametrize("method,query_path,reference_path", testdata_bad_input)
 def test_check_bad_user_input(method, query_path, reference_path):
