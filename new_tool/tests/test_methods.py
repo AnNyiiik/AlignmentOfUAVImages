85cd1be00ca95123a26dfacd262884c7f284d6ed
AnNyiiik <anna.nikolaevakons@gmail.com>
2024-09-13 17:35:52 +0300

    changed path inside of a test

--- new_tool/tests/test_methods.py
+++ new_tool/tests/test_methods.py
@@ -27,9 +27,9 @@ testdata_homography_search = [
 
 @pytest.mark.parametrize("method,query_path,reference_path", testdata_homography_search)
 def test_check_homography_search(method, query_path, reference_path):
-    absolute_path = Path("~/AlignmentOfUAVImages").expanduser()
+    absolute_path = os.path.abspath("AlignmentOfUAVImages/AlignmentOfUAVImages")
     folder_with_kpts = os.path.join(absolute_path, "feature_based_method_results")
-    os.mkdir(os.path.join(absolute_path, "feature_based_method_results"))
+    os.mkdir(folder_with_kpts)
     os.system(
         f'cp {os.path.join(absolute_path, "new_tool/tests/exp_data/matched_kpts_query")} {folder_with_kpts}'
     )
